d26dc9018cb66a81c579b1f2fc211993e8e147e5
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-24 17:08:05 +0800

    Add files via upload

---
+++ socalMedia.py
@@ -0,0 +1,30 @@
+# Using object Functions
+
+class User:
+    def __init__(self,firstName,lastName,likesCount,friendsName):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.likesCount = likesCount
+        self.friendsName = friendsName
+        
+    def display(self):
+        print(f"Firstname  : {self.firstName}")
+        print(f"Lastname   : {self.lastName}")
+        print(f"Likes count: {self.likesCount}")
+        print('Friends: ')
+        for friend in self.friendsName:
+            print(f"-{friend}")
+
+firstName = input("Enter firstname:")
+lastName = input('Enter lastname  :')
+likesCount = int(input('Enter Likes count: '))
+friendsName = input('Enter Friend name: ') 
+
+# to split the input into a list of friend names
+friendList = friendsName.split(',')
+
+user1 = User(firstName,lastName,likesCount,friendList)
+user1.display()       
+# user1 = User('Louwis Alfredo','JoC',20,['Jonathan','Bruce Lee'])
+# user1.display()
+
